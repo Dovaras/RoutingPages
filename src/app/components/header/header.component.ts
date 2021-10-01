2a9abeee6e6352cf503a3436594de9df22cefa7f
Arūnas Dovainis <arunas.dovainis@gmail.com>
2021-10-01 17:16:48 +0300

    Paspaudimas

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,13 +10,26 @@ export class HeaderComponent implements OnInit {
   constructor() {
   }
   ngOnInit(): void {
+    const ulQuery = document.querySelector("ul");
+    ulQuery?.addEventListener("click", handleClick);
 
-    const aQuery = document.querySelector("ul");
-        aQuery?.addEventListener("click", handleClick);
 
     function handleClick(this: HTMLElement) {
-        console.log("Clicked!");
-        this.removeEventListener("click", handleClick);
+
+      //const imgQuery = document.querySelector("img");
+      //const imgLink = imgQuery?.getAttribute('src');
+
+      var images = document.getElementsByTagName('img');
+      if (images[0].src === '../../../assets/images/statyba1.jpg') {
+          images[0].style.display = "none";
+          images[1].style.visibility = "visible";
+
+      } else {
+            images[1].src = '../../../assets/images/statyba2.jpg';
+            images[0].style.display = "none";
+            images[1].style.visibility = 'visible';
+          }
+          console.log(images.length);
+        }
     }
   }
-}
