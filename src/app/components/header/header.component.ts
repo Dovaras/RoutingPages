ca07227d67fb7dbf031aadc3de052cfd3c72ed65
Arūnas Dovainis <arunas.dovainis@gmail.com>
2021-10-01 17:57:18 +0300

    Bandymai

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -13,23 +13,28 @@ export class HeaderComponent implements OnInit {
     const ulQuery = document.querySelector("ul");
     ulQuery?.addEventListener("click", handleClick);
 
-
+    var images = document.getElementsByTagName('img');
+    images[1].style.display = "none"; //visibility = "hidden";
+    images[0].style.visibility = "visible";
+    var image = images[0];
     function handleClick(this: HTMLElement) {
 
+      var image = images[0];
       //const imgQuery = document.querySelector("img");
       //const imgLink = imgQuery?.getAttribute('src');
 
-      var images = document.getElementsByTagName('img');
-      if (images[0].src === '../../../assets/images/statyba1.jpg') {
-          images[0].style.display = "none";
-          images[1].style.visibility = "visible";
-
-      } else {
-            images[1].src = '../../../assets/images/statyba2.jpg';
-            images[0].style.display = "none";
-            images[1].style.visibility = 'visible';
-          }
-          console.log(images.length);
+      //if (images[0].src === '../../../assets/images/statyba1.jpg') {
+          images[1].style.display = "none"; //visibility = "hidden";
+          images[0].style.visibility = "visible";
+          image = images[0];
+          images[0] = images[1];
+          images[1] = image;
+      //} else {
+          //   images[0].src = '../../../assets/images/statyba2.jpg';
+          //   images[1].style.visibility = "hidden";
+          //   images[0].style.visibility = "visible";
+          // }
+          console.log("JAU lyg ir...");
         }
     }
   }
