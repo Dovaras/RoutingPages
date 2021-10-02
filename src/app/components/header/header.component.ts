d79a319fd7a9d06f1e901cdcbd1fea2358e655af
Arūnas Dovainis <arunas.dovainis@gmail.com>
2021-10-02 16:52:34 +0300

    Pries sekmadieni

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,16 +10,32 @@ export class HeaderComponent implements OnInit {
   constructor() {
   }
   ngOnInit(): void {
-    const buttonQuery = document.querySelector("button");
-    function handleClick(this: HTMLElement) {
+    const buttonQuery = document.querySelectorAll("button");
+    // const classAbout = document.getElementsByClassName('.about');
+    // var x = document.getElementsByTagName("button")[1];
 
+    function handleClick0(this: HTMLElement) {
 
-      //document.getElementsByClassName("home");
-      window.location.href = '../home/';
-
-      //"../home/home.component.html";
-      console.log("Pasispaudė!!");
+      console.log(buttonQuery[1].textContent);
+      if (buttonQuery[1].textContent === 'About') {
+        window.location.href = '../about';
+      }
+      else {
+        window.location.href = '../home/home.component.html';
+        window.location.reload();
+      }
     }
-    buttonQuery?.addEventListener("click", handleClick);
+    buttonQuery[1].addEventListener("click", handleClick0);
+
+
+    function handleClick1(this: HTMLElement) {
+      console.log(buttonQuery[0].textContent);
+      if (buttonQuery[0].textContent === 'Home') {
+        window.location.href = '../home/home.component.html';
+      }
+      else {
+        window.location.href = '../about/about.component.html';
+      }
+    } buttonQuery[0].addEventListener("click", handleClick1);
   }
 }
