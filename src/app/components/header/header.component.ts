c6ab8fca53342ce8fffbd11bb862949f6a31d83a
Arūnas Dovainis <arunas.dovainis@gmail.com>
2021-10-02 08:53:47 +0300

    Header changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,30 +11,45 @@ export class HeaderComponent implements OnInit {
   }
   ngOnInit(): void {
     const ulQuery = document.querySelector("ul");
-    ulQuery?.addEventListener("click", handleClick);
 
-    var images = document.getElementsByTagName('img');
-    images[1].style.display = "none"; //visibility = "hidden";
-    images[0].style.visibility = "visible";
-    var image = images[0];
+    //var image = images[0];
     function handleClick(this: HTMLElement) {
 
-      var image = images[0];
+      //onClick="mouseClicked()"
+      var number = 1;
+      var mouseClicked = function() {
+        console.log('mouseClick = ' + number);
+        number++;
+      }
+
+      var img = document.getElementsByTagName('img');
+      img[0].src = '../../../assets/images/statyba1.jpg';
+      img[0].style.visibility = 'hidden';
+      img[1].src = '../../../assets/images/statyba2.jpg';
+      img[1].style.visibility = 'visible';
+
+      ulQuery?.removeEventListener("click", handleClick);
+
+      //img[0].style.display = "none"; //visibility = "hidden";
+      // img[1].style.visibility = "visible";
+
+      //var image = images[0];
       //const imgQuery = document.querySelector("img");
       //const imgLink = imgQuery?.getAttribute('src');
 
-      //if (images[0].src === '../../../assets/images/statyba1.jpg') {
-          images[1].style.display = "none"; //visibility = "hidden";
-          images[0].style.visibility = "visible";
-          image = images[0];
-          images[0] = images[1];
-          images[1] = image;
-      //} else {
-          //   images[0].src = '../../../assets/images/statyba2.jpg';
-          //   images[1].style.visibility = "hidden";
-          //   images[0].style.visibility = "visible";
-          // }
+      // if (img[0].src === '../../../assets/images/statyba1.jpg') {
+      //     img[0].style.display = "none"; //visibility = "hidden";
+      //     img[1].style.visibility = "visible";
+      //     // image = img[0];
+      //     // img[0] = img[1];
+      //     // img[1] = image;
+      // } else {
+      //       img[1].src = '../../../assets/images/statyba2.jpg';
+      //       img[1].style.display = "none"; //visibility = "hidden";
+      //       img[0].style.visibility = "visible";
+      //     }
           console.log("JAU lyg ir...");
         }
+        ulQuery?.addEventListener("click", handleClick);
     }
   }
